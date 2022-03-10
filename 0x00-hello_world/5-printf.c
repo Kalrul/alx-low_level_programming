f095cc296befcffe86120c6025bcf12bba953165
Kalrul <prizsabertler@gmail.comgit config --global user.email prizsabertler@gmail.com>
2022-03-10 09:12:19 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Entry points
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  *
  */
+
 int main(void)
+
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art, \n");
-	return (0);
+
+		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
 }
