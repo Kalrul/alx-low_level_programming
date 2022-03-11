7c07d2e44e6ba2a422414517d257755134868803
Kalrul <prizsabertler@gmail.com>
2022-03-11 16:11:13 +0300

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - print if the number is positive, zero, or negative
- *
- * Description: using the main function
- * this program print "Prints the alphabets in lower case
- * Return: 0
- */*
+ * * main - Entry point
+ * *
+ * * Return: returns 0 when everything works fine
+ * *
+ * */
+
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-	putchar(ch);
-}
-putchar('\n');
-return (0);
+	int n;
+
+	for (n = 97; n <= 122; ++n)
+	putchar(n);
+	putchar(10);
+	return (0);
 }
