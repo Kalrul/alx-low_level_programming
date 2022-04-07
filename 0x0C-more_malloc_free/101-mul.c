b8bc52396898575a1b68a75bc8150340f99f16bd
Kalrul <prizsabertler@gmail.comgit config --global user.email prizsabertler@gmail.com>
2022-04-07 12:44:46 -0700

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+
 /**
- * _isnumber - checks if string is number
- *
+ * isnumber - checks if string is number
  * @s: string
  *
  * Return: 1 if number, 0 if not
  */
-int _isnumber(char *s)
+
+int isnumber(char *s)
 {
 	int i, check, d;
 
@@ -27,13 +28,14 @@ int _isnumber(char *s)
 }
 
 /**
- * _callocX - reserves memory initialized to 0
+ * callocX - reserves memory initialized to 0
  *
  * @nmemb: # of bytes
  *
  * Return: pointer
  */
-char *_callocX(unsigned int nmemb)
+
+char *callocX(unsigned int nmemb)
 {
 	unsigned int i;
 	char *p;
@@ -54,18 +56,19 @@ char *_callocX(unsigned int nmemb)
  * @argv: cmd line args
  * Return: No return
  */
+
 int main(int argc, char **argv)
 {
 	int i, j, l1, l2, lful, mul, add, ten, ten2, tl, zer = 0;
 	char *res;
 
-	if (argc != 3 || _isnumber(argv[1]) == 0 || _isnumber(argv[2]) == 0)
+	if (argc != 3 || isnumber(argv[1]) == 0 || isnumber(argv[2]) == 0)
 		printf("Error\n"), exit(98);
 	if (atoi(argv[1]) == 0 || atoi(argv[2]) == 0)
 		printf("0\n"), exit(0);
 	l1 = strlen(argv[1]), l2 = strlen(argv[2]);
 	lful = l1 + l2;
-	res = _callocX(lful);
+	res = callocX(lful);
 	if (res == 0)
 		printf("Error\n"), exit(98);
 	for (i = l2 - 1; i >= 0; i--)
