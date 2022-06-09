a7c3a566392054f1f6ad6b253f42b419f6483c71
Kalrul <prizsabertler@gmail.comgit config --global user.email prizsabertler@gmail.com>
2022-06-09 01:39:50 -0700

    the tasks added are from task 0 to task 4

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint_end - add node to end of doubly linked list
+ * @head: pointer to head of list
+ * @n: node data
+ * Return: address of new node, or Null if failed
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *last;
+
+	if (!head)
+		return (NULL);
+
+	/* malloc and set new node data */
+	new = malloc(sizeof(struct dlistint_s));
+	if (!new)
+		return (NULL);
+	new->n = n;
+
+	/* account for empty linked list */
+	if (*head == NULL)
+	{
+		*head = new;
+		new->next = NULL;
+		new->prev = NULL;
+		return (new);
+	}
+
